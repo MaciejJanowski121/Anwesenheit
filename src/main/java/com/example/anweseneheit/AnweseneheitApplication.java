5e3395b75ff85580f9bfb6501e8cd7958ba6e8d4
Maciej Janowski <maciejjanowski@Mac.fritz.box>
2026-06-04 15:01:48 +0200

    Initial project setup with Spring Boot and React

---
+++ src/main/java/com/example/anweseneheit/AnweseneheitApplication.java
@@ -0,0 +1,13 @@
+package com.example.anweseneheit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnweseneheitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnweseneheitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/anweseneheit/AnweseneheitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.anweseneheit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnweseneheitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
